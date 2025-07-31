6ccbfff6616ff299cb038b878742d54961b1ccdc
unknown <hangochung3006@gmail.com>
2025-07-31 15:55:05 +0700

    Update 15:55 ngày 31/07/2025

---
+++ bai1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Họ và tên: Hà Ngọc Hưng");
+            Console.WriteLine("Năm sinh: 2001");
+            Console.WriteLine("Giới tính: Nam");
+            Console.Write("Tuổi của bạn: " + 20);
+            Console.ReadLine();
+        }
+    }
+}
