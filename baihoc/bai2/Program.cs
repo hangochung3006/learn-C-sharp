b8ae610712ba3d1330616d7a4d84c87314cd828b
unknown <hangochung3006@gmail.com>
2025-07-31 17:02:50 +0700

    Update 17:02 ngày 31/07/2025

--- baihoc/bai2/Program.cs
+++ baihoc/bai2/Program.cs
@@ -10,6 +10,15 @@ namespace bai2
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            int soLuong; //khai báo biến
+            string hoTen = "Hà Ngọc Hưng"; //khởi tạo biến
+            byte tuoi; //khai báo biến
+            double diemToan = 8; //khởi tạo biến
+            float diemTB = 6.5f; //khởi tạo biến, riêng kiểu float thì phải có chữ f ở cuối
+            Console.WriteLine("Họ tên là {0}", hoTen);
+            Console.WriteLine("Họ tên là {0}, Điểm toán là {1}, Điểm trung bình là {2}", hoTen, diemToan, diemTB);
+            Console.ReadLine(); 
         }
     }
 }
