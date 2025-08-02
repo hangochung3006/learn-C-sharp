bd1c38ac2c09c0b6a5a2489292142caddd13cec0
unknown <hangochung3006@gmail.com>
2025-08-02 10:37:50 +0700

    Update 10:37 ngày 02/08/2025

---
+++ baihoc/bai4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Nhap so nguyen n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("So {0} la so {1}", n, (n % 2 == 0) ? "chan" : "le");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ baihoc/bai4.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1, Viết chương trình cho phép người dùng nhập vào số nguyên a
+            int a;
+            Console.WriteLine("Hay nhap a: ");
+            a = int.Parse(Console.ReadLine());
+            //2, Nhập a=5 , thực hiện các phép tính sau:
+            //2.A a+=3
+            Console.WriteLine("a += 3: " + (a += 3));
+            a = 5;
+            //2.B a-=5
+            Console.WriteLine("a -= 5: " + (a - +5));
+            a = 5;
+            //2.C a*=2
+            Console.WriteLine("a *= 2: " + (a *= 2));
+            a = 5;
+            //2.D a/=9
+            Console.WriteLine("a /= 9: " + (a /= 9));
+            a = 5;
+            //2.E a%=5
+            Console.WriteLine("a %= 5: " + (a %= 5));
+            a = 5;
+            //F cho b=2, rút gọn biểu thức a=a-(b+7);
+            int b = 2;
+            Console.WriteLine("a = a - (b + 7): " + (a -= (b + 7)));
+            Console.ReadKey();
+        }
+    }
+}
