76d205b27084d2471e9cfc78c0fa87954ece1c7d
chumeolc <chumeolv@gmail.com>
2025-08-04 19:59:53 +0700

    Update 19:59 ngày 04/08/2025

---
+++ baihoc/bai9.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai9._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("CHUONG TRINH TIM KIEM");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("1. Tim kiem theo ten");
+            Console.WriteLine("2. Tim kiem theo tac gia");
+            Console.WriteLine("3. Tim kiem theo nha xuat ban");
+            Console.WriteLine("4. Tim kiem theo tieu de");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ baihoc/bai9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //nhập vào 1 số, kiểm tra chẵn hay lẻ dùng switch case
+            int a;
+            Console.Write("Nhap so: ");
+            a = int.Parse(Console.ReadLine());
+            int div = a % 2;
+            switch (div)
+            {
+                case 0:
+                    Console.WriteLine("Day la so chan");
+                    goto case 1;
+                case 1:
+                    Console.WriteLine("Day la so le");
+                    break;
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
