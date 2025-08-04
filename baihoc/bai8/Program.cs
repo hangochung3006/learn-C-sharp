bce9dab7e7231152bb4c6673242361bb327f62c7
unknown <hangochung3006@gmail.com>
2025-08-04 17:31:26 +0700

    Update 17:31 ngày 04/08/2025

---
+++ baihoc/bai8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //nhập vào 1 số n, kiểm tra n là chẵn hay lẻ;
+            int n;
+            string a;
+            Console.Write("n = ");
+            n = int.Parse(Console.ReadLine());
+            a = (n % 2 == 0) ? "chan" : "le";
+            Console.WriteLine("n la so " + a);
+            Console.ReadKey();
+        }
+    }
+}
