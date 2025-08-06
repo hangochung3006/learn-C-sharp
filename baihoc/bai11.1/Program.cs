70833c4fd5d6ec1fe8b2648ca8bade384fe786e8
unknown <hangochung3006@gmail.com>
2025-08-06 16:56:12 +0700

    Update 16:55 ngày 06/08/2025

---
+++ baihoc/bai11.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai11._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {1,3,5,7,8,10,12,13};
+            int thang;
+            Console.Write("Hay nhap thang muon kiem tra: ");
+            thang = int.Parse(Console.ReadLine());
+            if (thang < 1 || thang > 12)
+            {
+                Console.WriteLine("Thang khong hop le");
+                return;
+            }
+            foreach (int i in a)
+            {
+                if (i == thang)
+                {
+                    Console.WriteLine("Thang {0} co 31 ngay", thang);
+                    break;
+                }
+                else if(i == 13)
+                {
+                    Console.WriteLine("Thang {0} co 30 ngay", thang);
+                }
+            }
+        }
+    }
+}
---
+++ baihoc/bai11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Xuất các số chẵn từ 0 - 10
+            int a = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    a += i;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Tong cac so chan tu 0 - 10 la: " + a);
+            Console.ReadKey();
+        }
+    }
+}
