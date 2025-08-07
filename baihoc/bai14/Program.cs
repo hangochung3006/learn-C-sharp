b3e010be8d80183319e9ed623f8881355155f0d0
unknown <hangochung3006@gmail.com>
2025-08-07 17:33:14 +0700

    Update 17:33 ngày 07/08/2025

---
+++ baihoc/bai14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char kyTu = 'a';
+            Console.WriteLine("Ky tu: " + kyTu);
+            Console.WriteLine(kyTu.GetType().ToString());
+            char kyTu2 = (char)64;
+            Console.WriteLine("Ky tu 2: " + kyTu2);
+            Console.ReadKey();
+        }
+    }
+}
