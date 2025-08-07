3f259b519d9cf72653ffa30bea035dcd75971481
unknown <hangochung3006@gmail.com>
2025-08-07 17:17:45 +0700

    Update 17:17 ngày 07/08/2025

---
+++ baihoc/bai12.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai12._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime birthday = new DateTime(2001, 06, 30);
+            Console.WriteLine("Ngay sinh cua toi la: " + birthday.ToString("dd/MM/yyyy"));
+
+            DateTime today = DateTime.Now;
+            Console.WriteLine("Bay gio dang la:" + today);
+
+            DateTime birthday2 = DateTime.Parse("2001/06/30");
+            Console.WriteLine("Ngay sinh cua toi la: " + birthday2.ToString("dd/MM/yyyy"));
+            //Viết chương trình nhập vào ngày sinh của bạn và in ra ngày sinh đó theo định dạng dd/MM/yyyy
+            //int year, day, month;
+            //Console.Write("Nhap vao nam sinh cua ban: ");
+            //year = int.Parse(Console.ReadLine());
+            //Console.Write("Nhap vao thang sinh cua ban: ");
+            //month = int.Parse(Console.ReadLine());
+            //Console.Write("Nhap vao ngay sinh cua ban: ");
+            //day = int.Parse(Console.ReadLine());
+            //DateTime ngaySinh = new DateTime(year, month, day);
+            //Console.WriteLine("Ngay sinh cua ban da nhap la: " + ngaySinh.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Nhap ngay sinh cua ban (ngay/thang/nam): ");
+            string s = Console.ReadLine();
+            DateTime birthday3 = DateTime.Parse(s);
+            Console.WriteLine("Ngay sinh ban vua nhap la: " + birthday3.ToString("dd/MM/yyyy"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ baihoc/bai13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            // (begin, sát end) begin không nhập sẽ mặc định = 0
+            //.Next để lấy kiểu số nguyên
+            int soNgauNhien = rd.Next(1, 100); // 1-99
+            Console.WriteLine("So ngau nhien may sinh la: " + soNgauNhien);
+            //.NextDouble để lấy kiểu số thực
+            double soThucNgauNhien = rd.NextDouble(); // 0 - sát 1
+            Console.WriteLine("So thuc ngau nhien: " + soThucNgauNhien);
+            // Lấy số thực ngẫu nhiên trong khoảng từ 1 đến 10
+            double soThucNgauNhien1Den10 = rd.NextDouble() + rd.Next(1,10); // 1 - 10
+            Console.WriteLine("So thuc ngau nhien trong khoang tu 1 - 10: " + soThucNgauNhien1Den10);
+            Console.ReadKey();
+        }
+    }
+}
