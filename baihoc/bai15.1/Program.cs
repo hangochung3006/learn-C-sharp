928085cabc9b565ea064c760474a1fb516d171a2
chumeolc <hangochung3006@gmail.com>
2025-08-10 18:47:13 +0700

    Update 18:46 ngày 10/08/2025

---
+++ baihoc/bai15.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai15._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao mot day ky tu: ");
+            string chuoi = Console.ReadLine();
+            char[] kyTu = chuoi.ToCharArray();
+            int kyTuThuong = 0, kyTuSo = 0, dauCach = 0;
+            foreach (char c in kyTu)
+            {
+                if (Char.IsLower(c))
+                    kyTuThuong++;
+                else if (Char.IsNumber(c))
+                    kyTuSo++;
+                else if (Char.IsWhiteSpace(c))
+                    dauCach++;
+            }
+            Console.WriteLine("Co tat ca {0} ky tu thuong, {1} ky tu so, {2} dau cach trong chuoi {3}", kyTuThuong, kyTuSo, dauCach, chuoi);
+        }
+    }
+}
--- baihoc/bai15/Program.cs
+++ baihoc/bai15/Program.cs
@@ -24,7 +24,7 @@ namespace bai15
 
             //Biến đổi chuỗi
             int i = 1234;
-            Console.WriteLine(i + " " +i.GetType().ToString());
+            Console.WriteLine(i + " " + i.GetType().ToString());
             string chuoiSo = i.ToString();
             Console.WriteLine(chuoiSo + " " + chuoiSo.GetType().ToString());
             string chuoiSo2 = i + "";
@@ -44,6 +44,59 @@ namespace bai15
             string doDai = "Ha Ngoc Hung";
             Console.WriteLine("Do dai chuoi la " + doDai.Length);
             Console.WriteLine("Ky tu thu nhat la: " + doDai[0]);
+
+            //So sánh chuỗi
+            string chuoi1 = "Ha Ngoc Hung";
+            string chuoi2 = "Ha Ngoc Hai";
+            string chuoi3 = "Ha Ngoc Hung";
+            int soSanh = chuoi1.CompareTo(chuoi2);
+            int soSanh2 = chuoi1.CompareTo(chuoi3);
+            Console.WriteLine(soSanh);
+            Console.WriteLine(soSanh2);
+
+
+            //Kiểm tra chuỗi con
+            string chuoi4 = "Sieu nhan Gao";
+            string chuoi5 = "Sieu nhan";
+            bool chuoiCon = chuoi4.Contains(chuoi5);
+            Console.WriteLine(chuoiCon);
+
+            //Hàm CopyTo
+            string chuoiCopyTo = "123456";
+            char[] hamCopyTo = new char[6]; //Khởi tạo mảng ký tự với kích thước 6
+            hamCopyTo[0] = 'a';
+            hamCopyTo[1] = 'b';
+            Console.WriteLine(hamCopyTo);
+            chuoiCopyTo.CopyTo(0, hamCopyTo, 2, 4); //Lấy 4 ký tự từ chuỗi chuoiCopyTo bắt đầu từ vị trí 0
+                                                    //và sao chép vào mảng hamCopyTo bắt đầu từ vị trí 2
+            Console.WriteLine(hamCopyTo);
+
+            //Hàm bool EndsWith(string value)
+            string fileNhac = "baihat.mp3";
+            bool kiemTra = fileNhac.EndsWith(".mp3"); //".mp3" != ".mp3 "
+            Console.WriteLine("File nhac co phai la mp3 khong? " + kiemTra);
+
+            //Gán chuỗi theo string.Format
+            int so = 14;
+            string chuoiFormat = string.Format("n = {0} và can bac hai cua no la {1}", so, Math.Sqrt(so));
+            Console.WriteLine(chuoiFormat);
+
+            //Hàm Equals
+            string chuoi6 = "Ha Ngoc Hung";
+            string chuoi7 = "Ha Ngoc Hai";
+            string chuoi8 = "Ha Ngoc Hung";
+            Console.WriteLine(chuoi6.Equals(chuoi7));
+            Console.WriteLine(chuoi6.Equals(chuoi8));
+
+            //Chèn thêm vào chuỗi
+            string chuoiCanChen = "Ha Ngoc Hung";
+            string chuoiSeChen = "Dang hoc C#";
+            //chèn chuỗi chuoiSeChen vào chuỗi chuoiCanChen tại vị trí 12
+            string chuoiDaChen1 = chuoiCanChen.Insert(12, " " + chuoiSeChen);
+            //chèn chuỗi chuoiSeChen vào chuỗi chuoiCanChen tại vị trí 6
+            string chuoiDaChen2 = chuoiCanChen.Insert(6, " " + chuoiSeChen);
+            Console.WriteLine(chuoiDaChen1);
+            Console.WriteLine(chuoiDaChen2);
             Console.ReadKey();
         }
     }
