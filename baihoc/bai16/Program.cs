740b5e4ca114e50965f4c54426b33d1c304b7421
unknown <hangochung3006@gmail.com>
2025-08-14 16:42:07 +0700

    Update 16:41 ngày 14/08/2025

--- baihoc/bai16/Program.cs
+++ baihoc/bai16/Program.cs
@@ -40,7 +40,42 @@ namespace bai16
             //Length
             int[] doDaiMang = new int[5] { 1, 2, 3, 4, 5 };
             Console.WriteLine("Chieu dai cua mang la: " + doDaiMang.Length);
+
+            //Truy xuất phần tử qua index
+            int[] mangXuat = { 1, 3, 5 };
+            Console.WriteLine(mangXuat[1]);
+            Console.WriteLine(mangXuat[mangXuat.Length-1]); //Phần từ cuối cùng của mảng
+
+            //Thay đổi giá trị phần tử của mảng bằng cách gọi index
+            int[] mangThayDoi = { 1, 3, 5 };
+            Console.WriteLine("Gia tri truoc khi doi: " + mangThayDoi[1]);
+            mangThayDoi[1] = 12;
+            Console.WriteLine("Gia tri sau khi thay doi: " + mangThayDoi[1]);
+
+            //Duyệt mảng for
+            int[] mang1 = new int[] { 1, 4, 5 };
+            for(int i = 0; i < mang1.Length; i++)
+            {
+                Console.WriteLine(mang1[i]);
+            }
+
+            //Duyệt mảng foreach
+            int[] mang2 = new int[] { 4, 8, 10, 26, 33 };
+            foreach (int so in mang2)
+            {
+                Console.WriteLine(so);
+            }
+
+            //Phép gán mảng
+            int[] mang3 = new int[] { 55, 22, 33, 44, 66 };
+            int[] mangGan = mang3;
+            Console.WriteLine(mang3[1]);
+            Console.WriteLine(mangGan[1]);
+            mangGan[1] = 4124124;
+            Console.WriteLine(mang3[1]);
+            Console.WriteLine(mangGan[1]);
             Console.ReadKey();
+
         }
     }
 }
