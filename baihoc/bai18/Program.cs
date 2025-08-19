39ac37e56c452a41c6e4d338715236128ec1be7d
chumeolc <hangochung3006@gmail.com>
2025-08-19 21:15:14 +0700

    Update 21:15 ngày 19/08/2025

--- baihoc/bai18/Program.cs
+++ baihoc/bai18/Program.cs
@@ -66,6 +66,86 @@ namespace bai18
             bool check = danhSachCheck.Contains(4);
             Console.WriteLine(check);
 
+            //GetRange(int index, int count)
+            List<int> list3 = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            List<int> danhSachGetRange = list3.GetRange(1, 3);
+            foreach (int item in danhSachGetRange)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            //IndexOf(<value>)
+            List<int> list4 = new List<int>() { 77, 48, 53, 54, 21 };
+            Console.WriteLine(list4.IndexOf(6));
+            Console.WriteLine(list4.IndexOf(54));
+
+            //Insert(int index, value)
+            List<int> list5 = new List<int>() { 1, 8, 2, 4, 5 };
+            list5.Insert(1, 2);
+            foreach(int item in list5)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            //InsertRange (int index, <Danh_sách_chèn_thêm>)
+            List<int> list6 = new List<int>() { 1, 8, 2, 4, 5 };
+            List<int> list7 = new List<int>() { 5, 2, 1 };
+            list6.InsertRange(2, list7);
+            foreach(int item in list6)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            //RemoveAt(int index)
+            List<int> list8 = new List<int>() { 1, 8, 2, 4, 5 };
+            list8.RemoveAt(1);
+            foreach(int item in list8)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            //RemoveRange(int index, int count)
+            List<int> list9 = new List<int>() { 1, 8, 2, 4, 5 };
+            list9.RemoveRange(2, 3);
+            foreach (int item in list9)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            //Reverse()
+
+            //Sort()
+
+            //BinarySearch
+
+            //Max, min
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
