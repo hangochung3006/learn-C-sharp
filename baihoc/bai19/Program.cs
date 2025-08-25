e0b4f6ff06048327cc967d584dff021bd10c6d92
chumeolc <hangochung3006@gmail.com>
2025-08-25 20:57:09 +0700

    Update 20:57 ngày 25/08/2025

--- baihoc/bai19/Program.cs
+++ baihoc/bai19/Program.cs
@@ -10,6 +10,15 @@ namespace bai19
     {
         static void Main(string[] args)
         {
+            //1. Khai báo Dic
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+
+            //2. Khai báo Dic có gán giá trị
+            Dictionary<int, string> dic2 = new Dictionary<int, string>() { {1, "Hưng"}, {2, "Huyền"} };
+
+
+
+            Console.ReadKey();
         }
     }
 }
