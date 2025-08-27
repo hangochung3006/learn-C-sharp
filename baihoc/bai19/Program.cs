24aaf5355e3ae0a6ae764bafaf7a3a332bd963b2
unknown <hangochung3006@gmail.com>
2025-08-27 17:02:24 +0700

    Update 17:02 ngày 27/08/2025

--- baihoc/bai19/Program.cs
+++ baihoc/bai19/Program.cs
@@ -16,6 +16,35 @@ namespace bai19
             //2. Khai báo Dic có gán giá trị
             Dictionary<int, string> dic2 = new Dictionary<int, string>() { {1, "Hưng"}, {2, "Huyền"} };
 
+            //3. Add
+            Dictionary<int, string> dic3 = new Dictionary<int, string>() { { 1, "Hung" }, { 2, "Huyen" } };
+            dic3.Add(3, "Mimi");
+            dic3.Add(4, "SamSam");
+            foreach(KeyValuePair<int,string> pTu in dic3)
+            {
+                Console.WriteLine(pTu);
+            }
+
+            //4. Duyệt dic
+            Dictionary<int, string> dic4 = new Dictionary<int, string>() { { 1, "Hung" }, { 2, "Huyen" } };
+            foreach (KeyValuePair<int, string> pTu in dic4)
+            {
+                Console.WriteLine(pTu);
+                Console.WriteLine("Key: {0}, Value: {1}", pTu.Key, pTu.Value);
+            }
+
+            //5. .ContainsKey()
+            Dictionary<int, string> dic5 = new Dictionary<int, string>() { { 1, "Hung" }, { 2, "Huyen" } };
+            bool result = dic5.ContainsKey(1);
+            bool result1 = dic5.ContainsKey(3);
+            Console.WriteLine("ket qua 1 = " + result);
+            Console.WriteLine("ket qua 2 = " + result1);
+
+
+
+
+
+
 
 
             Console.ReadKey();
