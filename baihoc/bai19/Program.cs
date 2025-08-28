31e2aa5fa5298e527ca10d47ade5a4260edc05a6
unknown <hangochung3006@gmail.com>
2025-08-28 17:10:30 +0700

    Update 17:10 ngày 28/08/2025

--- baihoc/bai19/Program.cs
+++ baihoc/bai19/Program.cs
@@ -40,6 +40,44 @@ namespace bai19
             Console.WriteLine("ket qua 1 = " + result);
             Console.WriteLine("ket qua 2 = " + result1);
 
+            //6. .ContainsValue()
+            Dictionary<int, string> dic6 = new Dictionary<int, string>() { { 1, "Hung" }, { 2, "Huyen" } };
+            bool result2 = dic6.ContainsValue("Hung");
+            bool result3 = dic6.ContainsValue("Mimi");
+            Console.WriteLine("ket qua 1 = " + result2);
+            Console.WriteLine("ket qua 2 = " + result3);
+
+            //7. <dic>[key]
+            Dictionary<int, string> dic7 = new Dictionary<int, string>() { { 1, "Hung" }, { 2, "Huyen" } };
+            string aGetValue = dic7[1];
+            Console.WriteLine(aGetValue);
+
+            //8. .Remove(key)
+            Dictionary<int, string> dic8 = new Dictionary<int, string>() { { 1, "Hung" }, { 2, "Huyen" } };
+            dic8.Remove(1);
+            foreach(KeyValuePair<int,string> pTu in dic8)
+            {
+                Console.WriteLine(pTu);
+            }
+
+            //9. .Clear()
+            Dictionary<int, string> dic9 = new Dictionary<int, string>() { { 1, "Hung" }, { 2, "Huyen" } };
+            dic9.Clear();
+            Console.WriteLine(dic9.Count());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
