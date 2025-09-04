e3d259b84c05252628f1fd629e96eec0d745bc64
unknown <hangochung3006@gmail.com>
2025-09-04 14:14:03 +0700

    Update 14:13 ngày 04/09/2025

--- baihoc/bai20/Program.cs
+++ baihoc/bai20/Program.cs
@@ -8,9 +8,49 @@ namespace bai20
 {
     internal class Program
     {
+        /// <summary>
+        /// Hàm này là để tính tổng nhé.
+        /// </summary>
+        /// <param name="a">Nhập a là số nguyên</param>
+        /// <param name="b">Nhập b là số nguyên</param>
+        /// <param name="c">Nhập c là số nguyên</param>
+        /// <returns>Trả về tổng a + b + c là số nguyên</returns>
+        static int tinhTong(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        static int tinhGiaiThua(int n)
+        {
+            int gt = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                gt *= i;
+            }
+            return gt;
+        }
+        static void xinChao(string m)
+        {
+            if(m == "Nam")
+            {
+                Console.WriteLine("Xin chao, minh la Boy");
+            }
+            else if(m == "Nu")
+            {
+                Console.WriteLine("Xin chao, minh la Girl");
+            }    
+        }
+        
         static void Main(string[] args)
         {
+            xinChao("Nam");
 
+            int kq = tinhTong(4, 2, 6);
+            Console.WriteLine("kq = "+kq);
+            //Xuất giai thừa
+            int k = 3;
+            int kq1 = tinhGiaiThua(k);
+            Console.WriteLine("Giai thua = "+kq1);
+            Console.ReadKey();
         }
     }
 }
