9e0602d3b8124f9ab2fda13f868124b0f399122a
Hưng <hangochung3006@gmail.com>
2025-10-09 15:41:18 +0700

    Update 15:41 ngày 09/10/2025

---
+++ baihoc/bai11.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai11._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap n = ");
+            int n = int.Parse(Console.ReadLine());
+            int dayN = 1, j = 1;
+            //for(int i = 1; i <= n; i++)
+            //{
+            //    dayN *= i;
+            //}
+            //Console.WriteLine("Tich N! = " + dayN);
+            while(j <= n)
+            {
+                dayN *= j;
+                j++;
+            }
+            Console.WriteLine("Tich N! = " + dayN);
+            Console.ReadLine();
+        }
+    }
+}
--- baihoc/bai21/Program.cs
+++ baihoc/bai21/Program.cs
@@ -8,8 +8,47 @@ namespace bai21
 {
     internal class Program
     {
+        static void ngoaiLe()
+        {
+            try
+            {
+                Console.WriteLine("Moi nhap vao ngay thang nam sinh: ");
+                string s = Console.ReadLine();
+                DateTime birthday = DateTime.Parse(s);
+                Console.WriteLine("Ngay sinh cua ban la: " + birthday.ToString("dd/MM/yyyy"));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Ket thuc chuong trinh");
+            }
+        }
+
+        static void ngoaiLe2()
+        {
+            Console.WriteLine("Moi nhap vao tu so: ");
+            int tuSo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Moi nhap vao mau so: ");
+            int mauSo = int.Parse(Console.ReadLine());
+            if (mauSo == 0)
+            {
+                throw new ArithmeticException("Mau so khong the bang 0");
+            }
+        }
         static void Main(string[] args)
         {
+            try
+            { 
+                ngoaiLe2();
+            }
+            catch(ArithmeticException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
