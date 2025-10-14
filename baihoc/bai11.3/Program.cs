72e0f7c09ee85101dc67afd15b995126f1ed020b
Hưng <hangochung3006@gmail.com>
2025-10-14 14:54:49 +0700

    Update 14:54 ngày 14/10/2025

---
+++ baihoc/bai11.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai11._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tong = 0;
+            string ketQua = "";
+            Console.WriteLine("Hay nhap so a = ");
+            int a = int.Parse(Console.ReadLine());
+            if(a % 2 == 0)
+            {
+                for(int i = 0; i <= a; i+=2)
+                {
+                    tong += i;
+                    if(i == a)
+                    {
+                        ketQua += i;
+                        break;
+                    }
+                    else { 
+                        ketQua += i + " + "; 
+                    }
+                }
+                Console.WriteLine("Tong cac so le tu 0 -> a = {0} = {1}", ketQua, tong);
+            }
+            else
+            {
+                Console.WriteLine("Toi khong tinh so le, bye bye");
+            }
+        }
+    }
+}
