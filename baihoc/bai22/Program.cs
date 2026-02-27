f30313fcff3f3cad2f7a347f56574b7901af679c
chumeolc <hangochung3006@gmail.com>
2026-02-27 21:09:19 +0700

    Update 21:09 ngày 27/02/2026

--- baihoc/bai22/Program.cs
+++ baihoc/bai22/Program.cs
@@ -12,13 +12,22 @@ namespace bai22
         {
 
             SinhVien sinhVien1 = new SinhVien();
-            Console.WriteLine(sinhVien1);
-            Console.WriteLine(sinhVien1.ToString());
+            SinhVien sinhVien2 = new SinhVien(2, "Nguyen Thanh Huyen", 25, "CNTT1", 15);
 
-            Console.WriteLine(sinhVien1.MaSV);
-            Console.WriteLine(sinhVien1.TenSV);
-            Console.WriteLine(sinhVien1.Tuoi);
-            Console.WriteLine(sinhVien1.Lop);
+            Console.WriteLine(sinhVien1.Tong(2, 3));
+            Console.WriteLine(sinhVien1.Tong(1, 2, 3, 4));
+            Console.WriteLine(sinhVien1.Tong(1.2, 2.3));
+
+            sinhVien1.xuatThongTin();
+            Console.WriteLine("\n");
+            sinhVien2.xuatThongTin();
+
+            //Console.WriteLine(sinhVien1);
+            //Console.WriteLine(sinhVien1.ToString());
+            Console.WriteLine(sinhVien1.Tong(2, 3));
+            Console.WriteLine(sinhVien1.Tong(1, 2, 3, 4));
+            Console.WriteLine(sinhVien1.Tong(1.2, 2.3));
+
             Console.ReadKey();
 
         }
--- baihoc/bai22/SinhVien.cs
+++ baihoc/bai22/SinhVien.cs
@@ -15,25 +15,30 @@ namespace bai22
         private string tenSV;
         private int tuoi;
         private string lop;
+        private float diemThi;
         #endregion
         //Gán giá trị mặc định cho đối tượng
+        #region Constructor
         public SinhVien()
         {
             this.maSV = 1;
             this.tenSV = "Ha Ngoc Hung";
             this.tuoi = 25;
             this.lop = "CNTT1";
+            this.diemThi = 7;
         }
         //Khởi tạo giá trị cho đối tượng do người dùng truyền vào
-        //public SinhVien(int maSV, string tenSV, int tuoi, string lop)
-        //{
-        //    this.maSV = maSV;
-        //    this.tenSV = tenSV;
-        //    this.tuoi = tuoi;
-        //    this.lop = lop;
-        //}
-
+        public SinhVien(int maSV, string tenSV, int tuoi, string lop, float diemThi)
+        {
+            this.maSV = maSV;
+            this.tenSV = tenSV;
+            this.tuoi = tuoi;
+            this.lop = lop;
+            this.diemThi = diemThi;
+        }
+        #endregion
         //Khai báo Properties để có thể truy xuất dữ liệu
+        #region Properties
         public int MaSV
         {
             get { return maSV; }
@@ -54,10 +59,50 @@ namespace bai22
             get { return lop; }
             set { lop = value; }
         }
+        public float DiemThi
+        {
+            get { return diemThi; }
+            set { diemThi = value; }
+        }
+        #endregion
         //Phương thức ToString 
         public override string ToString()
         {
             return this.MaSV + "\t" + this.TenSV;
+        }
+        //Suppor method
+        private bool checkDiemThi()
+        {
+            return (this.diemThi >= 0 && this.diemThi <= 10);
+        }
+        //Service method
+        public void xuatThongTin()
+        {
+            Console.WriteLine("Ma SV: " + this.MaSV);
+            Console.WriteLine("Ten SV: " + this.TenSV);
+            Console.WriteLine("Tuoi: " + this.Tuoi);
+            Console.WriteLine("Lop: " + this.Lop);
+            if (checkDiemThi())
+            {
+                Console.WriteLine("Diem thi: " + this.DiemThi);
+            }
+            else
+            {
+                Console.WriteLine("Diem thi khong hop le!");
+            }
+        }
+        //Overloading method
+        public int Tong (int a, int b)
+        {
+            return a + b;
+        }
+        public int Tong(int a, int b, int c, int d)
+        {
+            return a + b + c + d;
+        }
+        public double Tong(double a, double b)
+        {
+            return a + b;
         }
     }
 }
